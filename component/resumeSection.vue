2ecc4d9471e0b0fa1c50c0bda4e82c24f4e42d10
Preston Stosur-Bassett <preston@stosur.info>
2017-08-09 05:53:23 -0400

    added the resume section

--- component/resumeSection.vue
+++ component/resumeSection.vue
@@ -1,10 +1,39 @@
 <template>
   <div>
-    <h1>Resume Section</h1>
-    <p>This is some content.</p>
-    <p>This is some more content.</p>
-    <p>Even more content over here.</p>
-    <p>Okay, were done now.</p>
+    <h1 class="header-with-action">Resume</h1>
+    <a href="Preston_Wang-Stosur-Bassett_Resume.pdf" class="button button-primary">Download My Resume</a>
+    <div class="row">
+      <div class="six columns">
+        <h3>Education</h3>
+        <div class="info-group">
+          <h5>B.S. Computer Science - <i>Kalamazoo College</i></h5>
+          <p><b>2014 - 2018</b> | Senior studying computer science and Chinese with a 3.6 GPA. Studied abroad one year in China at Cpaital Normal University in Beijing and Harbin Institute of Technology in Harbin.</p>
+        </div>
+        <div class="info-group">
+          <h5>Highschool - <i>Glenbard West Highschool</i></h5>
+          <p><b>2011 - 2014</b> | Enrolled in all honors and AP classes. On high honor roll with a weighted 5.3 GPA. Vice President Chinese National Honors Society. Involved in political science and Chinese clubs.</p>
+        </div>
+      </div>
+      <div class="six columns">
+        <h3>Experience</h3>
+        <div class="info-group">
+          <h5>Freelance Web Developer</h5>
+          <p><b>2014 - Present</b> | Developed engaging online experiences for a variety of independent businesses using modern web technologies.</p>
+        </div>
+        <div class="info-group">
+          <h5>Front-End Developer - <i>MCON Beijing</i></h5>
+          <p><b>2016</b> | Developed WeChat apps for coporate clients in China using Angular and WeUI. Gained valuable experience working in international team using Agile Development Methodology.
+        </div>
+        <div class="info-group">
+          <h5>Manager, Salesman &amp; Technician - <i>CPR Cell Phone Repair</i></h5>
+          <p><b>2015 - 2016</b> | Managed the Kalamazoo franchise location, repaired a variety of mobile electronics and increased sales with a "no-pressure" sales style.</p>
+        </div>
+        <div class="info-group">
+          <h5>Software Engineer - <i>Floydware LLC</i></h5>
+          <p><b>2010 - 2013<b> | Internationalized software for use in other languages and developed Android mobile app for cloud scheduling software company. Valuable experience working in office setting; acquired coding "good-practices" by developing within a team.
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,4 +41,14 @@
 </script>
 
 <style scoped>
+.info-group p {
+  padding-top: 0px;
+}
+.info-group h5 {
+  padding-bottom: 0px;
+  margin-bottom: 8px;
+}
+.header-with-action {
+  margin-bottom: 8px;
+}
 </style>
