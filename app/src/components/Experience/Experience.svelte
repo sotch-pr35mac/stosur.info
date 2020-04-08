15a1ad6e92fe118ea749819cf237b6d692a406f8
Preston Wang-Stosur-Bassett <preston@stosur.info>
2020-04-07 23:51:19 -0400

    Added Experience and Education information

--- app/src/components/Experience/Experience.svelte
+++ app/src/components/Experience/Experience.svelte
@@ -1,62 +1,55 @@
 <script>
 import SectionContent from '../SectionContent/SectionContent.svelte';
 import SectionTitle from '../SectionTitle/SectionTitle.svelte';
-import JobCard from './JobCard.svelte';
+import ListItemDescription from '../ListItemDescription/ListItemDescription.svelte';
 
 const experiences = [
 	{
+		id: 1,
 		display: true,
 		startDate: '2018',
 		endDate: 'Present',
 		title: 'Senior Software Engineer',
-		company: 'Clinc',
-		description: 'Developed features and resolved issues on the Product Engineering team building a conversational AI web platform. Architected conversational AI for clients. Frew a team from zero to six and mentored junior engineers as teh lead of the AI Experience Development team.'
+		name: 'Clinc',
+		description: 'Developed features and resolved issues on the Product Engineering team building a conversational AI web platform. Architected conversational AI for clients. Frew a team from zero to six and mentored junior engineers as teh lead of the AI Experience Development team.',
+		link: 'https://clinc.com/'
 	},
 	{
+		id: 2,
 		display: true,
 		startDate: '2016',
 		endDate: '2016',
 		title: 'Front-End Developer',
-		company: 'MCON Beijing',
-		description: 'Developed WeChat apps for corporate clients in China using Angular and WeUI. Gained insights into developing for the Chinese market while working on a remote international team.'
+		name: 'MCON Beijing',
+		description: 'Developed WeChat apps for corporate clients in China using Angular and WeUI. Gained insights into developing for the Chinese market while working on a remote international team.',
+		link: 'https://www.mcon-group.com/'
 	},
 	{
+		id: 3,
 		display: true,
 		startDate: '2015',
 		endDate: '2016',
 		title: 'Manager',
-		company: 'CPR Cell Phone Repair',
-		description: 'Managed teh Kalamazoo franchise location of five people, repaired a variety of mobile electronics, and increased sales with a "no-pressure" sales style.'
+		name: 'CPR Cell Phone Repair',
+		description: 'Managed teh Kalamazoo franchise location of five people, repaired a variety of mobile electronics, and increased sales with a "no-pressure" sales style.',
+		link: 'https://www.cellphonerepair.com/'
 	},
 	{
+		id: 4,
 		display: true,
 		startDate: '2010',
 		endDate: '2013',
 		title: 'Junior Software Engineer',
-		company: 'Floydware, LLC',
-		description: 'Internationalized software for use in other languages and developed Android mobile app for cloud scheduling software company'
+		name: 'Floydware, LLC',
+		description: 'Internationalized software for use in other languages and developed Android mobile app for cloud scheduling software company',
+		link: 'https://www.rosysalonsoftware.com/'
 	}
 ];
 </script>
-
-<style>
-.experience-grid {
-	display: grid;
-	grid-template-columns: repeat(2, 1fr);
-	grid-gap: 10px;
-	grid-auto-rows: minmax(100px, auto);
-}
-</style>
 
 <SectionContent color="light">
 	<SectionTitle>
 		<h1>Experience</h1>
 	</SectionTitle>
-	<div class="experience-grid">
-		{#each experiences as experience}
-			{#if experience.display}
-				<JobCard positionDescription={experience}/>
-			{/if}
-		{/each}
-	</div>
+	<ListItemDescription items={experiences} showLink={true} />
 </SectionContent>
---
+++ app/src/components/ListItemDescription/ListItemDescription.svelte
@@ -0,0 +1,89 @@
+<script>
+export let items;
+export let showLink = false;
+
+let activeIndex = 0;
+const getItemList = () => items.map(i => i.name);
+const changeActiveIndex = index => {
+	activeIndex = index;
+}
+</script>
+
+<style>
+.list-item-description--container {
+	width: 100%;
+	display: flex;
+	justify-content: center;
+}
+.list-item-description {
+	display: flex;
+	align-items: flex-start;
+	max-width: 1000px;
+}
+.list-item-description--list {
+	list-style-type: none;
+	padding: var(--space) 0;
+	margin-top: 0 var(--space--extra-large) 0 0;
+	border-right: 1.5px solid var(--text-color--light);
+	min-width: max-content;
+}
+.list-item-description--list-item {
+	padding: var(--space--small);
+	margin: var(--space--extra-large);
+	text-align: right;
+}
+.list-item-description--list-item--active {
+	border-bottom: 1.5px solid var(--text-color--light);
+}
+.list-item-description--list-item--content {
+	text-transform: uppercase;
+}
+.list-item-description--list-item--content:hover {
+	border-bottom: 1.5px solid var(--text-color--light);
+	transition-property: border-color;
+	transition-duration: var(--transition-duration);
+	cursor: pointer;
+}
+.list-item-description--detail {
+	margin-left: var(--space--extra-large);
+}
+.list-item-description--detail--title {
+	margin: var(--space);
+}
+.list-item-description--detail--subtitle {
+	margin: var(--space);
+	color: var(--text-color--light);
+}
+.list-item-description--detail--description {
+	margin: var(--space);
+}
+.list-item-description--detail--link {
+	text-decoration: none;
+	color: var(--color--primary-end);	
+}
+.list-item-description--detail--link:hover {
+	border-bottom: 1.5px solid var(--color--primary-end);
+}
+</style>
+
+<div class="list-item-description--container">
+<div class="list-item-description">
+	<ul class="list-item-description--list">
+		{#each getItemList() as itemName, i}
+			<li class="list-item-description--list-item" on:click={() => changeActiveIndex(i)}>
+				<span class="list-item-description--list-item--content" class:list-item-description--list-item--active="{i == activeIndex}">{itemName}</span>
+			</li>	
+		{/each}
+	</ul>
+	<div class="list-item-description--detail">
+		<h1 class="list-item-description--detail--title">
+			{ items[activeIndex].title }
+			{#if showLink}
+				@&nbsp;<a class="list-item-description--detail--link" href="{items[activeIndex].link}" target="_blank">{items[activeIndex].name}</a>
+			{/if}
+		</h1>
+		<h3 class="list-item-description--detail--subtitle">{ items[activeIndex].startDate }&nbsp;-&nbsp;{ items[activeIndex].endDate }</h3>
+		<p class="list-item-description--detail--description">{ items[activeIndex].description }</p>
+	</div>
+</div>
+</div>
